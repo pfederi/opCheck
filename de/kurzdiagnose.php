8ea70743730369ee87c64e049da31813b524b65a
Patrick Federi <patrick@federi.com>
2014-11-18 22:30:45 +0100

    div

--- de/kurzdiagnose.php
+++ de/kurzdiagnose.php
@@ -1,5 +1,16 @@
 <?php include("../includes/header.inc.php"); ?>
 
+<div class="row">
+	<div class="large-12 medium-12 small-12 columns">
+		<h1>Kurzdiagnose</h1>
+		<form action="" method="" accept-charset="utf-8" data-abide>
+			<label>Choose Your Favorite</label>
+				<input type="radio" name="pokemon" value="Red" id="pokemonRed"><label for="pokemonRed">Red</label>
+				<input type="radio" name="pokemon" value="Blue" id="pokemonBlue"><label for="pokemonBlue">Blue</label>
+			<button type="submit">Submit</button>
+		</form>
+	</div>
+</div>
 
 <?php include("../includes/footer.inc.php"); ?>
 
