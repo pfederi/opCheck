cb2c5b0d8328785c8a8af1c2eeabc5f395ff1017
Patrick Federi <patrick@federi.com>
2014-11-18 20:41:51 +0100

    Diverse Anpassungen

---
+++ includes/header.inc.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>opCheck</title>
+    <link rel="stylesheet" href="css/app.css" />
+    <link rel="stylesheet" href="css/layout.css" />
+    <script src="bower_components/modernizr/modernizr.js"></script>
+  </head>
+  <body>
