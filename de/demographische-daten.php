39af1a099bf1e1681bc53b1f44204b89a38b9400
katering <kate@katering.de>
2014-11-18 22:39:49 +0100

    headline und 1. reihe

--- de/demographische-daten.php
+++ de/demographische-daten.php
@@ -1,6 +1,36 @@
 <?php include("../includes/header.inc.php"); ?>
 
 
+
+
+  <div class="row">
+    <div class="large-12 columns">
+    	<h1>Demographische Daten</h1>
+    	<form>
+      		<label>Anrede</label>
+        	<input type="text" placeholder="large-12.columns" />
+
+				
+  				<div class="row">
+  				   	<div class="large-4 columns">        	
+      					<label>...</label>
+        				<input type="text"/>
+        			</div>
+  				   	<div class="large-4 columns">            			
+        				<label>...</label>
+        				<input type="text"/>
+        			</div>
+  				   	<div class="large-4 columns">    
+        				 <label>...</label>
+        				<input type="text"/>
+        			</div>
+        		</div>	
+    	</form>
+    </div>
+  </div>
+
+
+
 <?php include("../includes/footer.inc.php"); ?>
 
 
