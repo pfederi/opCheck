176cee972c9c407a52b510ec941a4c0e5c7a4290
Patrick Federi <patrick@federi.com>
2014-11-21 10:35:40 +0100

    Diverses
    Include Files erstellt. Bitte in include Files arbeiten.

---
+++ de/zweitmeinung.php
@@ -0,0 +1,18 @@
+<?php include("../includes/header.inc.php"); ?>
+
+<!--1 Demographische Daten -->
+<?php include("../includes/formular/demographische-daten.inc.php"); ?>
+
+<!--2 Kurzdiagose -->
+<?php include("../includes/formular/kurzdiagnose.inc.php"); ?>
+
+<!--3 Befund -->
+<?php include("../includes/formular/befund.inc.php"); ?>
+
+<!--4 Befundbilder -->
+<?php include("../includes/formular/befundbilder.inc.php"); ?>
+
+<!--5 zweitmeinungsarzt -->
+<?php include("../includes/formular/zweitmeinungsarzt.inc.php"); ?>
+
+<?php include("../includes/footer.inc.php"); ?>
---
+++ includes/formular/befund.inc.php
@@ -0,0 +1,5 @@
+<div class="row">
+	<div class="large-12 columns">
+  		<h1>3 - Befund</h1>
+  	</div>
+</div>
