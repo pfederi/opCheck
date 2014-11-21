cb3bfc4010d81e3d209199dd97ac8b128d6c29bf
katering <kate@katering.de>
2014-11-21 11:11:10 +0100

    3-Befundbilder neu. Kleine Korrekturen an 1 und 2.

--- de/index.php
+++ de/index.php
@@ -3,7 +3,7 @@
 <div class="row surgeon">
 	<div class="large-12 columns large-centered">
 		<h1>Lorem Ipsum dolor sit amet</h1>
-		<a href="zweitmeinung">Zweitmeinung bestellen</a>
+		<a href="/de/zweitmeinung.php">Zweitmeinung bestellen</a>
 	</div>
 </div>
 
