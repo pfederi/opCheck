4c57899f924d659ce209f1e783323034ae8515a7
Patrick Federi <patrick@federi.com>
2014-11-21 12:39:23 +0100

    einzelne css

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>opCheck</title>
     <link rel="stylesheet" href="../css/app.css" />
+    <link rel="stylesheet" href="../css/layout-kate.css" />
+    <link rel="stylesheet" href="../css/layout-darko.css" />
     <link rel="stylesheet" href="../css/layout.css" />
     <script src="../bower_components/modernizr/modernizr.js"></script>
   </head>
