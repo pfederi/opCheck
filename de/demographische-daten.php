564a5217c4b4e9daa83128176fa27064174331cf
Patrick Federi <patrick@federi.com>
2014-11-21 09:54:41 +0100

    Divs geschlossen. Ansonsten saubere Arbeit

--- de/demographische-daten.php
+++ de/demographische-daten.php
@@ -2,11 +2,11 @@
 
 <!--Section 1 -->
 
-   <div class="row">
-    <div class="large-12 columns">
-      <h1>1- Kurzdiagnose</h1>
-      <form>
-          <div class="row">
+<div class="row">
+	<div class="large-12 columns">
+	  <h1>1- Kurzdiagnose</h1>
+	  <form>
+	      <div class="row">
               <div class="large-4 columns">         
                 <label>Fachbereich</label>
                 <select>
@@ -43,15 +43,14 @@
                 </label>
               </div>
           </div> 
-          <div class="row">
+	      <div class="row">
               <div class="large-12 columns">
                 <label>Wurde Ihnen vom Spezialisten eine OP empfohlen?</label>
                 <input type="radio" name="op-ja" value="Ja" id="op-ja"><label for="op-ja">Ja</label>
                 <input type="radio" name="op-nein" value="Nein" id="op-nein"><label for="op-nein">Nein</label>
               </div>
           </div>
-
-          <div class="row">
+	      <div class="row">
             <div class="large-6 columns">
               <label>Gründe für eine Zweitmeinung</label>
               <input id="checkbox1" type="checkbox"><label for="checkbox1">Ich bin unsicher</label>
@@ -63,92 +62,7 @@
               </label>
             </div>
           </div>
-<!-- -->
-          <div class="row">
-              <div class="large-10 columns">
-                
-              </div>
-              <div class="large-2 columns">
-                <a href="#" class="button [tiny small large]">Speichern</a>
-              </div>
-
-<!-- -->
-
-      </form>
-    </div>
-  </div>
-
-
-<!--Section 2 -->
-
-  <div class="row">
-    <div class="large-12 columns">
-    	<h1>2 - Demographische Daten</h1>
-    	<form>				
-  				<div class="row">
-  				   	<div class="large-2 columns">        	
-      					<label>Anrede</label>
-        				<input type="text"/>
-        			</div>
-  				   	<div class="large-5 columns">            			
-        				<label>Vorname</label>
-        				<input type="text"/>
-        			</div>
-  				   	<div class="large-5 columns">    
-        				 <label>Name</label>
-        				<input type="text"/>
-        			</div>
-        	</div>	
-
-          <div class="row">
-              <div class="large-5 columns">         
-                <label>Strasse</label>
-                <input type="text"/>
-              </div>
-              <div class="large-1 columns">                 
-                <label>Nr.</label>
-                <input type="text"/>
-              </div>
-              <div class="large-2 columns">                 
-                <label>PLZ</label>
-                <input type="text"/>
-              </div>
-              <div class="large-4 columns">    
-                 <label>Ort</label>
-                <input type="text"/>
-              </div>
-          </div>  
-
-          <div class="row">
-              <div class="large-4 columns">         
-                <label>Mobil</label>
-                <input type="text"/>
-              </div>
-              <div class="large-4 columns">                 
-                <label>Telefon</label>
-                <input type="text"/>
-              </div>
-              <div class="large-4 columns">    
-                 <label>E-Mail</label>
-                <input type="text"/>
-              </div>
-          </div>              
-
-          <div class="row">
-              <div class="large-4 columns">         
-                <label>Geburtsdatum</label>
-                <input type="text"/>
-              </div>
-              <div class="large-4 columns">                 
-                <label>Grösse</label>
-                <input type="text"/>
-              </div>
-              <div class="large-4 columns">    
-                 <label>Gewicht</label>
-                <input type="text"/>
-              </div>
-            </div>  
-          <div class="row">
+	      <div class="row">
               <div class="large-10 columns">
                 
               </div>
@@ -156,19 +70,96 @@
                 <a href="#" class="button [tiny small large]">Speichern</a>
               </div>
           </div>
+	  </form>
+	</div>
+</div>
+
+
+<!--Section 2 -->
+
+<div class="row">
+	<div class="large-12 columns">
+		<h1>2 - Demographische Daten</h1>
+		<form>				
+			<div class="row">
+			   	<div class="large-2 columns">        	
+					<label>Anrede</label>
+					<input type="text"/>
+			   	</div>
+			   	<div class="large-5 columns">            			
+					<label>Vorname</label>
+					<input type="text"/>
+				</div>
+			   	<div class="large-5 columns">    
+					<label>Name</label>
+					<input type="text"/>
+				</div>
+			</div>
+			<div class="row">
+	        	<div class="large-5 columns">         
+	            	<label>Strasse</label>
+					<input type="text"/>
+				</div>
+				<div class="large-1 columns">                 
+	            	<label>Nr.</label>
+					<input type="text"/>
+				</div>
+				<div class="large-2 columns">                 
+	            	<label>PLZ</label>
+					<input type="text"/>
+				</div>
+				<div class="large-4 columns">    
+	            	<label>Ort</label>
+					<input type="text"/>
+				</div>
+			</div>  
+			<div class="row">
+				<div class="large-4 columns">         
+					<label>Mobil</label>
+					<input type="text"/>
+				</div>
+				<div class="large-4 columns">                 
+			    	<label>Telefon</label>
+					<input type="text"/>
+				</div>
+				<div class="large-4 columns">    
+			    	<label>E-Mail</label>
+					<input type="text"/>
+				</div>
+			</div>              
+			<div class="row">
+            	<div class="large-4 columns">         
+                	<label>Geburtsdatum</label>
+					<input type="text"/>
+            	</div>
+				<div class="large-4 columns">                 
+                	<label>Grösse</label>
+					<input type="text"/>
+				</div>
+				<div class="large-4 columns">    
+             	 	<label>Gewicht</label>
+			 	 	<input type="text"/>
+				</div>
+            </div>  
+			<div class="row">
+            	<div class="large-10 columns">
+				</div>
+				<div class="large-2 columns">
+                <a href="#" class="button [tiny small large]">Speichern</a>
+				</div>
+			</div>
+          </div>
     	</form>
+	</div>
+</div>
 
 <!--Section 3 -->
 
-   <div class="row">
-    <div class="large-12 columns">
-      <h1>3 - Befund</h1>
-    </div>
-  </div>
-
-
-    </div>
-  </div>
+<div class="row">
+	<div class="large-12 columns">
+  		<h1>3 - Befund</h1>
+  	</div>
+</div>
 
 
 
