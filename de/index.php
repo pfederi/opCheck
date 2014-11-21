f43dda356f9a85bbbe265f222ed017c5b5ec423c
katering <kate@katering.de>
2014-11-21 16:55:38 +0100

    Infotexte Homepage, Umbenennung Links im Header

--- de/index.php
+++ de/index.php
@@ -28,24 +28,24 @@
 		</div>
 		<div class="row">
 			<div class="large-4 columns">
-				<img src="/images/icons/check.svg" alt="check"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+				<img src="/images/icons/check.svg" alt="check"><p>Neutrale und individuelle Betrachung Ihres Falles</p>
 			</div>
 			<div class="large-4 columns">
-				<img src="/images/icons/check.svg" alt="check"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+				<img src="/images/icons/check.svg" alt="check"><p>Beratung durch unabhängige und anerkannte Spezialisten mit FMH-Ausweis</p>
 			</div>
 			<div class="large-4 columns">
-				<img src="/images/icons/check.svg" alt="check"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+				<img src="/images/icons/check.svg" alt="check"><p>Keine Telemedizin - Sie sprechen direkt mit dem Spezialisten</p>
 			</div>
 		</div>
 		<div class="row">
 			<div class="large-4 columns">
-				<img src="/images/icons/check.svg" alt="check"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+				<img src="/images/icons/check.svg" alt="check"><p>Ausführlicher und verständlicher Abschlussbericht</p>
 			</div>
 			<div class="large-4 columns">
-				<img src="/images/icons/check.svg" alt="check"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+				<img src="/images/icons/check.svg" alt="check"><p>Übernahme der Konsultationskosten durch die Krankenkasse</p>
 			</div>
 			<div class="large-4 columns">
-				<img src="/images/icons/check.svg" alt="check"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+				<img src="/images/icons/check.svg" alt="check"><p>Niedrige Schutzgebühr von CHF 25 bei durchgeführter Beratung </p>
 			</div>
 		</div>
 	</div>
@@ -58,7 +58,7 @@
 		<h3>Wie funktioniert opCheck?</h3>
 		<div class="row" data-equalizer>
 			<div class="large-5 columns panel" data-equalizer-watch>
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+				Der Patient übermittelt seine Befund-Daten an opCheck.
 			</div>
 			<div class="large-2 columns number" data-equalizer-watch>
 				<div><span>1</span></div>
@@ -71,12 +71,12 @@
 				<div><span>2</span></div>
 			</div>
 			<div class="large-5 columns panel" data-equalizer-watch>
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+				Die Daten werden vom opCheck-Team auf Vollständigkeit geprüft. Anschliessend werden passende Spezialisten gesucht und dem Patienten vorgeschlagen.
 			</div>
 		</div>
 		<div class="row">
 			<div class="large-5 columns panel" data-equalizer-watch>
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+				Der Patient wählt einen Spezialisten aus und vereinbart einen Termin.
 			</div>
 			<div class="large-2 columns number" data-equalizer-watch>
 				<div><span>3</span></div>
@@ -89,13 +89,13 @@
 				<div><span>4</span></div>
 			</div>
 			<div class="large-5 columns panel" data-equalizer-watch>
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+				Spezialist und Patient treffen sich persönlich zum ausführlichen Zweitmeinungsgespräch.
 			</div>
 			
 		</div>
 		<div class="row">
 			<div class="large-5 columns panel" data-equalizer-watch>
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+				Abschliessend erhält der Patient einen ausführlichen schriftlichen Befund.	
 			</div>
 			<div class="large-2 columns number" data-equalizer-watch>
 				<div><span>5</span></div>
@@ -103,7 +103,6 @@
 			<div class="large-5 columns" data-equalizer-watch></div>
 		</div>
 	</div>
-	<span class="intro-link"></span>
 </section>
 <hr>
 <!-- Wer ist opCheck -->
@@ -127,21 +126,6 @@
 		</div>
 	</div>
 </section>
- <script>
-$(document).ready(function () {
-	$('.intro-link').on('click',function()
-		{
-		if ($('.ablauf').css('height') == '460px')
-		$('.ablauf').animate({height: $('.ablauf')[0].scrollHeight}, 500,"swing",function(){
-		$('.ablauf').css('padding-bottom','120px'),
-		$('.intro-link').css('background-image','url(/images/icons/arrow-up.svg)')});
-		else
-			$('.ablauf').animate({height: "460px"}, 500,"swing",function(){
-			$('.intro-link').css('background-image','url(/images/icons/arrow-down.svg)')});
-		}
-	);
-});
-</script>
 
 <?php include("../includes/footer.inc.php"); ?>
 
