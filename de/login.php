f52937e3384dd911e2ec6bfc7e8a49279729f5b0
dejot <dajergovic@gmail.com>
2014-11-24 23:24:47 +0100

    nun ist login.php gleich wie anmelden.php

--- de/login.php
+++ de/login.php
@@ -2,6 +2,11 @@
 
 <section class="row">
 	<div class="large-6 columns large-centered">
+		<div class="spinner hidden">
+			<p>Einen Augenblick bitte...</p>
+			<img src="/images/design/spinner.gif" alt="Progress Anzeige">
+		</div>
+
 		<h1>Anmelden</h1>
 		<p>Loggen Sie sich mit der Angabe Ihrer E-Mailadresse und ihrer Mobiltelefonnummer ein.</p>
 		<?php include("../includes/login.inc.php"); ?>
