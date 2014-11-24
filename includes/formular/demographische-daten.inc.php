c3518fc98670f782ba4625b4f501f8d06c061c9d
dejot <dajergovic@gmail.com>
2014-11-24 01:54:25 +0100

    1 Demographische Daten leicht angepasst, wenn man die Hausnummer eingegeben hatte, konnte man diese nicht sehen, zu wenig breit.

--- includes/formular/demographische-daten.inc.php
+++ includes/formular/demographische-daten.inc.php
@@ -1,47 +1,56 @@
 <div class="row">
 	<div class="large-12 columns">
 <!-- 		<h1>1 - Demographische Daten</h1> -->
-		<form>				
+		<form>	
+		<div class="margin-bottom-15">
+			
 			<div class="row">
-			   	<div class="large-2 columns">
+			   	<div class="large-2 columns end">
 					<label>Anrede</label>
 					<select>
-					<option value="#">...</option>	
+<!-- 					<option value="#">...</option>	 -->
                   	<option value="herr">Herr</option>
                   	<option value="frau">Frau</option>
                   </select>
 			   	</div>
 			   	<div class="large-5 columns">            			
 					<label>Vorname</label>
-					<input type="text"/ placeholder="...">
+					<input type="text"/ placeholder="..." value="Beat">
 				</div>
-			   	<div class="large-5 columns">    
+			   	<div class="large-5 columns end">    
 					<label>Name</label>
-					<input type="text"/ placeholder="...">
+					<input type="text"/ placeholder="..." value="Iseli">
 				</div>
-			</div>
+			</div>	
+
+		</div>			
+		<div class="margin-bottom-15">
 			<div class="row">
 	        	<div class="large-5 columns">         
 	            	<label>Strasse</label>
 					<input type="text"/ placeholder="...">
 				</div>
-				<div class="large-1 columns">                 
+				<div class="large-2 columns end">                 
 	            	<label>Nr.</label>
 					<input type="text"/ placeholder="...">
 				</div>
+			</div>
+			<div class="row">
 				<div class="large-2 columns">                 
 	            	<label>PLZ</label>
 					<input type="text"/ placeholder="...">
 				</div>
-				<div class="large-4 columns">    
+				<div class="large-4 columns end">    
 	            	<label>Ort</label>
 					<input type="text"/ placeholder="...">
 				</div>
-			</div>  
+			</div> 			
+		</div>
+ 		<div class="margin-bottom-15">
 			<div class="row">
 				<div class="large-4 columns">         
 					<label>Mobil</label>
-					<input type="text"/ placeholder="...">
+					<input type="text"/ placeholder="..." value="078 123 45 67">
 				</div>
 				<div class="large-4 columns">                 
 			    	<label>Telefon</label>
@@ -49,9 +58,11 @@
 				</div>
 				<div class="large-4 columns">    
 			    	<label>E-Mail</label>
-					<input type="text"/ placeholder="...">
+					<input type="text"/ placeholder="..." value="beat.iseli@persona.ch">
 				</div>
-			</div>              
+			</div> 			
+		</div>			  
+		<div class="margin-bottom-15">
 			<div class="row">
             	<div class="large-4 columns">         
                 	<label>Geburtsdatum</label>
@@ -65,7 +76,9 @@
              	 	<label>Gewicht</label>
 			 	 	<input type="text"/ placeholder="...">
 				</div>
-            </div>  
+            </div> 			
+		</div>             
+ 
     	</form>
 	</div>
 </div>
